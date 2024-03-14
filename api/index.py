0f2c31fff56b6f3e408e7916c427a3aa1a2eb55e
ffabianesouzaa <ffabianesouzaa@gmail.com>
2024-03-14 15:59:51 -0300

    first event on click add

--- api/index.py
+++ api/index.py
@@ -11,16 +11,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/ocupacoes")
-def list_ocup():
-    # Creating a database connection
-    connection = sqlite3.connect('../db/dbthesis.db')
+@app.get("/list/campi/ocup/{campi}")
+def list_campi_ocup(campi):
+    conn = sqlite3.connect('../db/dbthesis.db')
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name'] 
+    db = conn.cursor()
 
-    # Creating a cursor
-    cursor = connection.cursor()
-    cursor.execute("pragma enconding=UTF8")
-    cursor.execute("SELECT Ocupação FROM Ocupacoes")
-    data = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return json.dumps(data)
+    rows = db.execute('''
+        SELECT Ocupação FROM Ocupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0)
+        UNION SELECT Ocupação FROM NaoOcupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0) 
+    ''', (campi, campi,)).fetchall()
+    
+    conn.commit()
+    conn.close()
+    
+    # return json.dumps( [dict(ix) for ix in rows] ) #CREATE JSON
+    return rows
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    async function listOcup() {
-        const data = await fetch('http://127.0.0.1:8000/ocupacoes', {
-          method: 'GET',
-        })
-          .then((res) => res.json())
-          .then((data) => data)
-          .catch((error) => console.log(error))
-      
-        //document.querySelector('#cargos').innerHTML = ''
-        console.log(data)
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  const campiButtons = document.querySelectorAll('#campi button')
 
-    listOcup()
+  async function handleCampiButtonClick(event) {
+    const ocupacoesContainer = document.querySelector('#ocupacoes')
+    ocupacoesContainer.innerHTML = ''
+
+    campiButtons.forEach((button) => button.classList.remove('active'))
+
+    const campi = event.target
+    campi.classList.add('active')
+    console.log(campi)
+
+    const data = await fetch('http://127.0.0.1:8000/list/campi/ocup/' + campi.id, {
+      method: 'GET'
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((error) => console.log(error))
+
+    console.log(data)
+
+    data.forEach((vaga) => {
+      const element = `
+        <span class="item" id="${vaga['CodCBO']}"><button>${vaga['Ocupação']}</button></span>
+      `
+      ocupacoesContainer.innerHTML += element
+    })
+  }
+
+  campiButtons.forEach((button) => button.addEventListener('click', handleCampiButtonClick))
+
 })
