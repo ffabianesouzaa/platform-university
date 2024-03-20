94979120be6a286add547a5b51ce4cb8b8ba4cd3
ffabianesouzaa <ffabianesouzaa@gmail.com>
2024-03-20 16:57:36 -0300

    ocup button ok

--- api/index.py
+++ api/index.py
@@ -37,3 +37,24 @@ def list_campi_ocup(campi):
     conn.close()
     
     return { 'rows': resultsRows, 'ocupadas': resultsOcupadas, 'nocupadas': resultsNocupadas }
+
+@app.get("/list/campi/ocup/total/{campi}/{ocupacoes}")
+def list_campi_ocup(campi, ocupacoes):
+    conn = sqlite3.connect('../db/dbthesis.db')
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+       
+    ocupadas = db.execute('''
+        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM Ocupadas WHERE Cidade = ? AND Ocupação = ?
+    ''', (campi, ocupacoes,)).fetchall()
+    resultsOcupadas = ocupadas[0][0]
+    
+    nocupadas = db.execute('''
+        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM NaoOcupadas WHERE Cidade = ? AND Ocupação = ?
+    ''', (campi, ocupacoes)).fetchall()
+    resultsNocupadas = nocupadas[0][0]
+    
+    conn.commit()
+    conn.close()
+    
+    return { 'ocupadas': resultsOcupadas, 'nocupadas': resultsNocupadas }
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const campiButtons = document.querySelectorAll('#campi button')
 
-  async function handleCampiButtonClick(event) {
-    const ocupacoesContainer = document.querySelector('#ocupacoes')
-    ocupacoesContainer.innerHTML = ''
+  async function handleOcupButtonClick(event) {
+    const ocupButtons = document.querySelectorAll('#ocupacoes button')
+    ocupButtons.forEach((button) => button.classList.remove('active'))
+    const ocupButton = event.target
+    ocupButton.classList.add('active')
+    console.log(ocupButton)
 
-    campiButtons.forEach((button) => button.classList.remove('active'))
+    const campiId = ocupButton.getAttribute('data-campi-id')
 
-    const campi = event.target
-    campi.classList.add('active')
-    console.log(campi)
-
-    const data = await fetch('http://127.0.0.1:8000/list/campi/ocup/' + campi.id, {
+    const data = await fetch(`http://127.0.0.1:8000/list/campi/ocup/total/${campiId}/${ocupButton.id}`, {
       method: 'GET'
     })
-      .then((res) => res.json()) 
+      .then((res) => res.json())
       .then((data) => data)
       .catch((error) => console.log(error))
 
     console.log(data)
 
-    data.rows.forEach((vaga) => {
-      const element = `
-        <span class="item" id="${vaga}"><button>${vaga}</button></span>
-      `
-      ocupacoesContainer.innerHTML += element
-
-    })
-
-    document.querySelector('#bar-ocupadas').style.width = `${data.ocupadas}`
     const total = data.ocupadas + data.nocupadas
     const percentageOcupadas = (data.ocupadas * 100) / total
     const percentageNocupadas = 100 - percentageOcupadas
@@ -39,6 +28,51 @@ document.addEventListener('DOMContentLoaded', function () {
     barOcupadas.style.width = `${percentageOcupadas}%`
     barNocupadas.style.width = `${percentageNocupadas}%`
 
+  }
+
+  const campiButtons = document.querySelectorAll('#campi button')
+  async function handleCampiButtonClick(event) {
+    const ocupacoesContainer = document.querySelector('#ocupacoes')
+    ocupacoesContainer.innerHTML = ''
+    let ocupButtons = document.querySelectorAll('#ocupacoes button')
+    if (ocupButtons.length) {
+      ocupButtons.forEach((button) => button.removeEventListener('click', handleOcupButtonClick)) 
+    }
+    campiButtons.forEach((button) => button.classList.remove('active'))
+
+    const campi = event.target
+    campi.classList.add('active')
+    console.log(campi)
+
+    const data = await fetch('http://127.0.0.1:8000/list/campi/ocup/' + campi.id, {
+      method: 'GET'
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((error) => console.log(error))
+
+    console.log(data)
+
+    data.rows.forEach((vaga) => {
+      const element = `
+        <span class="item"><button id="${vaga}" data-campi-id="${campi.id}">${vaga}</button></span>
+      `
+      ocupacoesContainer.innerHTML += element
+
+    })
+
+    const total = data.ocupadas + data.nocupadas
+    const percentageOcupadas = (data.ocupadas * 100) / total
+    const percentageNocupadas = 100 - percentageOcupadas
+
+    const barOcupadas = document.querySelector('#bar-ocupadas')
+    const barNocupadas = document.querySelector('#bar-nocupadas')
+
+    barOcupadas.style.width = `${percentageOcupadas}%`
+    barNocupadas.style.width = `${percentageNocupadas}%`
+    
+    ocupButtons = document.querySelectorAll('#ocupacoes button')
+    ocupButtons.forEach((button) => button.addEventListener('click', handleOcupButtonClick))
   }
 
   campiButtons.forEach((button) => button.addEventListener('click', handleCampiButtonClick))
