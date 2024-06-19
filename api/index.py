0329a0d1c6b7081e448625929308f690be1081f8
ffabianesouzaa <ffabianesouzaa@gmail.com>
2024-06-19 15:54:31 -0300

    base de dados atualizada para evitar duplicatas. Iniciada parte do top5.

--- api/index.py
+++ api/index.py
@@ -21,27 +21,47 @@ def list_campi_ocup(campi):
     
     # Seleciona a cidade e preenche as ocupações que tiveram pelo menos uma vaga ofertada
     rows = db.execute('''
-        SELECT Ocupação FROM Ocupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0)
-        UNION SELECT Ocupação FROM NaoOcupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0) 
+        SELECT Ocupação FROM Ocupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0 OR Março2022 > 0 OR Abril2022 > 0 OR Maio2022 > 0 OR Junho2022 > 0 OR Julho2022 > 0 OR Agosto2022 > 0 OR Setembro2022 > 0 OR Outubro2022 > 0 OR Novembro2022 > 0 OR Dezembro2022 > 0)
+        UNION SELECT Ocupação FROM NaoOcupadas WHERE Cidade = ? AND (Janeiro2022 > 0 OR Fevereiro2022 > 0 OR Março2022 > 0 OR Abril2022 > 0 OR Maio2022 > 0 OR Junho2022 > 0 OR Julho2022 > 0 OR Agosto2022 > 0 OR Setembro2022 > 0 OR Outubro2022 > 0 OR Novembro2022 > 0 OR Dezembro2022 > 0) 
     ''', (campi, campi,)).fetchall()
     resultsRows = [row[0] for row in rows]
     
     # Seleciona o total de vagas ocupadas (todas as ocupações) da cidade selecionada
     ocupadas = db.execute('''
-        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM Ocupadas WHERE Cidade = ? 
+        SELECT SUM(Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) FROM Ocupadas WHERE Cidade = ? 
     ''', (campi,)).fetchall()
     resultsOcupadas = ocupadas[0][0]
     
     # Seleciona o total de vagas não ocupadas (todas as ocupações) da cidade selecionada
     nocupadas = db.execute('''
-        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM NaoOcupadas WHERE Cidade = ? 
+        SELECT SUM(Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) FROM NaoOcupadas WHERE Cidade = ? 
     ''', (campi,)).fetchall()
-    resultsNocupadas = nocupadas[0][0]
+    resultsNocupadas = nocupadas[0][0] #O resultado da primeira linha e coluna
+    
+    #Seleciona as 5 maiores vagas ocupadas
+    topOcupadas = db.execute('''
+        SELECT Ocupação, (Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) AS ocup_sum FROM Ocupadas WHERE Cidade = ?
+        ORDER BY ocup_sum DESC LIMIT 5 
+    ''', (campi,)).fetchall()
+    resultsTopocupadas = topOcupadas
+    
+    #Seleciona as 5 maiores vagas não ocupadas
+    topNocupadas = db.execute('''
+        SELECT Ocupação, (Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) AS ocup_sum FROM NaoOcupadas WHERE Cidade = ?
+        ORDER BY ocup_sum DESC LIMIT 5 
+    ''', (campi,)).fetchall()
+    resultsTopNocupadas = topNocupadas
     
     conn.commit()
     conn.close()
     
-    return { 'rows': resultsRows, 'ocupadas': resultsOcupadas, 'nocupadas': resultsNocupadas }
+    return { 
+        'rows': resultsRows, 
+        'ocupadas': resultsOcupadas, 
+        'nocupadas': resultsNocupadas, 
+        'topOcup': resultsTopocupadas, 
+        'topNocup': resultsTopNocupadas
+    }
 
 # Consulta para preencher termômetro de vagas e skills de acordo com a ocupação selecionada
 @app.get("/list/campi/ocup/total/{campi}/{ocupacoes}")
@@ -52,13 +72,13 @@ def list_campi_ocup(campi, ocupacoes):
     
     # Seleciona o total de vagas ocupadas (todas as ocupações) da cidade e ocupação selecionadas
     ocupadas = db.execute('''
-        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM Ocupadas WHERE Cidade = ? AND Ocupação = ?
+        SELECT SUM(Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) FROM Ocupadas WHERE Cidade = ? AND Ocupação = ?
     ''', (campi, ocupacoes,)).fetchall()
     resultsOcupadas = ocupadas[0][0]
     
     # Seleciona o total de vagas não ocupadas (todas as ocupações) da cidade e ocupação selecionadas
     nocupadas = db.execute('''
-        SELECT SUM(Janeiro2022 + Fevereiro2022) FROM NaoOcupadas WHERE Cidade = ? AND Ocupação = ?
+        SELECT SUM(Janeiro2022 + Fevereiro2022 + Março2022 + Abril2022 + Junho2022 + Julho2022 + Agosto2022 + Setembro2022 + Outubro2022 + Novembro2022 + Dezembro2022) FROM NaoOcupadas WHERE Cidade = ? AND Ocupação = ?
     ''', (campi, ocupacoes)).fetchall()
     resultsNocupadas = nocupadas[0][0]
     
@@ -72,3 +92,5 @@ def list_campi_ocup(campi, ocupacoes):
     conn.close()
     
     return { 'ocupadas': resultsOcupadas, 'nocupadas': resultsNocupadas, 'hard': resultsHard }
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  //Ao clicar em um botão
+  //Ao clicar em um botão de ocupação
   async function handleOcupButtonClick(event) {
+    //pegar botões de ocupação
     const ocupButtons = document.querySelectorAll('#ocupacoes button')
+    //remove a classe ativa dos demais
     ocupButtons.forEach((button) => button.classList.remove('active'))
+    //identifica o botão que foi criado (o elemento que gerou o evento)
     const ocupButton = event.target
+    //inclui a classe active 
     ocupButton.classList.add('active')
+    // acompanhar execucação no dev tools (pode remover)
     console.log(ocupButton)
 
+    //Pega a div que contém as skills
     const hardSkills = document.querySelector('#hard')
+    // Limpa todas as vezes que selecionar uma nova ocupação
     hardSkills.innerHTML = ''
 
+    //Pegando um elemento no html (querySelector)
     const barOcupadas = document.querySelector('#bar-ocupadas')
     const barNocupadas = document.querySelector('#bar-nocupadas')
 
     const barLabelOcupadas = document.querySelector('#ocupadas-label')
     const barLabelNocupadas = document.querySelector('#nocupadas-label')
 
+    // Pega o ID do campi através do atributo data-campi-id
     const campiId = ocupButton.getAttribute('data-campi-id')
 
+    // Chamada pra API (python) que retorna a quantidade de vagas ocupadas/nocupadas do banco de dados
     const data = await fetch(`http://127.0.0.1:8000/list/campi/ocup/total/${campiId}/${ocupButton.id}/`, {
       method: 'GET'
     })
-      .then((res) => res.json())
-      .then((data) => data)
-      .catch((error) => console.log(error))
+      .then((res) => res.json()) // sucesso > transforma em json
+      .then((data) => data) // retorna o sucesso/dados da etapa anterior
+      .catch((error) => console.log(error)) // erro
 
     console.log(data)
 
+    //Calculos
     const total = data.ocupadas + data.nocupadas
     const percentageOcupadas = (data.ocupadas * 100) / total
     const percentageNocupadas = 100 - percentageOcupadas
 
-
-
+    //Prencher no html conforme o calculo
     barOcupadas.style.width = `${percentageOcupadas}%`
     barNocupadas.style.width = `${percentageNocupadas}%`
 
+    // A label só aparece se o dado existir
     if (percentageOcupadas < 1) {
       barLabelOcupadas.style.display = 'none'
     }
@@ -51,12 +62,15 @@ document.addEventListener('DOMContentLoaded', function () {
       barLabelNocupadas.style.display = 'block'
     }
 
+    // Preenche no HTML o tamanho
     barLabelOcupadas.style.width = `${percentageOcupadas}%`
     barLabelNocupadas.style.width = `${percentageNocupadas}%`
 
+    // Preenche no HTML o valor
     barOcupadas.innerHTML = data.ocupadas
     barNocupadas.innerHTML = data.nocupadas
 
+    // Mapeando os dados recebidos (data) e transforma em lista <li>
     hardSkills.innerHTML = data.hard.map((skill) => `<li>${skill}</li>`).join('');
 
   }
@@ -128,10 +142,12 @@ document.addEventListener('DOMContentLoaded', function () {
     barOcupadas.innerHTML = data.ocupadas
     barNocupadas.innerHTML = data.nocupadas
 
+    //Seleciona todos os botão de ocupação
     ocupButtons = document.querySelectorAll('#ocupacoes button')
+    //Cria o evento de click em cada botão de ocupação
     ocupButtons.forEach((button) => button.addEventListener('click', handleOcupButtonClick))
   }
-
+  //Clique no botão de cidade/campus
   campiButtons.forEach((button) => button.addEventListener('click', handleCampiButtonClick))
 
 })
